7d780c2dcc8407bba443c13336b2232f935b0214
MacBook Pro <macbookpro@macbooks-mbp.nvstu.edu>
2017-03-01 17:38:27 -0500

    Microblog

---
+++ src/com/novauc/User.java
@@ -0,0 +1,16 @@
+package com.novauc;
+
+import java.util.ArrayList;
+
+/**
+ * Created by macbookpro on 2/28/17.
+ */
+public class User {
+    public User(String name) {
+        this.name = name;
+    }
+
+    String name;
+    ArrayList<String>post = new ArrayList<>();
+
+}
